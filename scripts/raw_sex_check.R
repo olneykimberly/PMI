537299cbe0a9d4c8884ae2a42797b39a0ca22fb6
olneykimberly <olneykimberly@gmail.com>
2024-07-16 11:33:25 -0500

    DESeq2 filter expresssion

--- scripts/raw_sex_check.R
+++ scripts/raw_sex_check.R
@@ -29,6 +29,25 @@ for (sample_id in sample_ids) {
   seurat_obj <- NormalizeData(seurat_obj)
   
   # Generate violin plot
+  sex_linked_plot <- VlnPlot(seurat_obj, features = c("Mapt", "Rbfox3", "Ctsb"), ncol = 4, pt.size = 0) +
+    plot_annotation(title = sample_id)
+  
+  # Save plot as PDF
+  output_path <- paste0(base_path, "results/Mapt/", sample_id, "_violin.pdf")
+  pdf(output_path, width = 7.5, height = 3.5)
+  print(sex_linked_plot)
+  dev.off()
+}
+
+for (sample_id in sample_ids) {
+  # Create Seurat object
+  data_path <- paste0(base_path, "cellranger/", sample_id, "/outs/filtered_feature_bc_matrix.h5")
+  seurat_obj <- CreateSeuratObject(Read10X_h5(data_path))
+  
+  # Normalize data
+  seurat_obj <- NormalizeData(seurat_obj)
+  
+  # Generate violin plot
   sex_linked_plot <- VlnPlot(seurat_obj, features = c("Xist", "Uty", "Ddx3y", "Ctsb"), ncol = 4, pt.size = 0) +
     plot_annotation(title = sample_id)
   
